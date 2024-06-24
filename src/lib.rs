0e2a5784148372167c2e7ee2770f814753b405ac
Luca Barbato <lu_zero@gentoo.org>
2024-06-24 16:34:27 +0200

    feat: Implement uuid7

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,23 @@
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyDict};
 
-/// Formats the sum of two numbers as string.
+/// Compute an UUID v7 using the current time
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn uuid7() -> PyResult<Py<PyAny>> {
+    let uuid = uuid::Uuid::now_v7();
+    Python::with_gil(|py| {
+        let kwargs = PyDict::new_bound(py);
+        kwargs.set_item("int", uuid.as_u128())?;
+        let pyuuid = PyModule::import_bound(py, "uuid")?;
+        pyuuid
+            .getattr("UUID")?
+            .call((), Some(&kwargs))
+            .map(|u| u.unbind())
+    })
 }
 
-/// A Python module implemented in Rust.
+/// UUID v7
 #[pymodule]
 fn oruuid(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(uuid7, m)?)?;
     Ok(())
 }
